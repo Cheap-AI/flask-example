0c147081d7262639a67f6cc235a5ebd9d58b05cf
James Kim <Jameshk1992@gmail.com>
2022-08-02 23:43:33 -0400

    hello guys

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from secrets_ import SOURCE,MEANINGOFLIFE
-
+import secrets
 from flask import Flask
 
 
-print(f" {SOURCE}and seriously {MEANINGOFLIFE}")
-
+# print(f" {SOURCE}and seriously {MEANINGOFLIFE}")
+# print(secrets.randbits(7))
 application = Flask(__name__)
 app = application
 
 
 @app.route('/')
 def hello_world():
-    return f'You say I say {SOURCE}'
+    return f'You say I say {SOURCE} and {MEANINGOFLIFE}'
